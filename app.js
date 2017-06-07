d980d5f8d397dbeddd5947d63c67af2ba5af2683
Sergey Hovakimyan <sergey.hovakimyan@gmail.com>
2017-06-07 13:08:38 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const Gpio = require('chip-gpio').Gpio;
+const NodeWebcam = require("node-webcam");
+const SerialPort = require("serialport");
+
+const btn = new Gpio(7, 'in', 'both', {debounceTimeout: 500});
+
+
+const exit = () => {
+	btn.unexport();
+	process.exit();
+};
+
+process.on('SIGINT', exit);
+
+const opts = {
+	width: 1280,
+	height: 720,
+	quality: 100,
+	delay: 0,
+	saveShots: true,
+	output: "jpeg",
+	device: false,
+	callbackReturn: "location",
+	verbose: false
+};
+
+const Webcam = NodeWebcam.create(opts);
+
+Webcam.capture("test_picture", function (err, data) {
+});
+
+NodeWebcam.capture("test_picture", opts, function (err, data) {
+
+});
+
+//Return type with base 64 image
+
+const c_opts = {
+	callbackReturn: "base64"
+};
+
+
+const port = new SerialPort("/dev/ttyS0", {
+	baudRate: 9600,
+}, (err) => {
+	console.log('open');
+	let n = 0;
+	// setInterval(()=>{
+	// 	n++;
+	// 	port.write('number is ' + n + '\n');
+	// }, 1000);
+
+	btn.watch((err, value) => {
+		port.write('taking a picture number' + n++ + '\n');
+		NodeWebcam.capture("test_picture", c_opts, function (err, data) {
+			console.log(data);
+			const image = "<img src='" + data + "'>";
+
+		});
+
+
+	});
+
+});
+
+port.on('data', function (data) {
+	console.log('Data: ' + data);
+});
